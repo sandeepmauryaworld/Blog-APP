d9a17f9f4db652991326004c680fb3e467ecf4b9
Sandeepmauryaworld <85479357+sandeepmauryaworld@users.noreply.github.com>
2023-08-01 15:38:02 +0530

    database setup

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `connected to mongodb database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
+  } catch (error) {
+    console.log("Mongo Connect Error".bgRed.white);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // env config
 dotenv.config();
+
+// mongodb coneection
+connectDB();
 //rest object
 
 const app = express();
