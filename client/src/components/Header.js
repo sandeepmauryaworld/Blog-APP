62a6096b432fa4137a36649701f2c93d094b06f1
Sandeepmauryaworld <85479357+sandeepmauryaworld@users.noreply.github.com>
2023-08-02 15:36:38 +0530

    rotuing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,46 @@
-import React from "react";
-import { Box, AppBar, Button, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 const Header = () => {
+  const [value, setValue] = useState();
   return (
     <>
       <AppBar position="sticky">
         <Toolbar>
           <Typography variant="h4">My Blog App</Typography>
+          <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+              <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+            </Tabs>
+          </Box>
           <Box display={"flex"} marginLeft="auto">
-            <Button sx={{ margin: 1, color: "white" }}>Login </Button>
-            <Button sx={{ margin: 1, color: "white" }}>Register </Button>
+            <Button
+              sx={{ margin: 1, color: "white" }}
+              LinkComponent={Link}
+              to="/login"
+            >
+              Login{" "}
+            </Button>
+            <Button
+              sx={{ margin: 1, color: "white" }}
+              LinkComponent={Link}
+              to="/register"
+            >
+              Register{" "}
+            </Button>
             <Button sx={{ margin: 1, color: "white" }}>Logout </Button>
           </Box>
         </Toolbar>
